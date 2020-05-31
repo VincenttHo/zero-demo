dee94ca7f0ffafbb4e635788ccd23e1da5ae3629
heyingcheng <252130586@qq.com>
2020-05-31 16:42:32 +0800

    增加敌人行走ai，增加敌人受伤流血效果

---
+++ Assets/Scripts/BloodEffect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodEffect : MonoBehaviour
+{
+
+    public float destroyTime = 1f;
+
+    void Start()
+    {
+        Destroy(gameObject, destroyTime);    
+    }
+
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,39 +5,49 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    /**属性*/
+    // 生命点
     public int healthyPoint = 3;
-
+    // 攻击伤害值
     public int damage = 1;
+    // 伤害闪烁时间
+    public float damageFlashTime = 0.1f;
+    // 精灵原始颜色（用于闪烁还原）
+    protected Color originColor;
 
-    private SpriteRenderer spriteRenderer;
+    /**组件*/
+    protected SpriteRenderer spriteRenderer;
+    public GameObject bloodEffect;
 
-    public float flashTime = 0.2f;
-
-    private Color originColor;
-
-    void Start()
+    protected void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         originColor = spriteRenderer.color;
     }
 
+    // 受伤方法
     public void GetDamage(int damage)
     {
         healthyPoint -= damage;
         FlashColor();
+        // 增加流血效果（试验，并不好看）
+        //bloodEffect.transform.position = new Vector3(transform.position.x, transform.position.y + 1, -5);
+        //Instantiate(bloodEffect);
         if (healthyPoint <= 0)
         {
             Destroy(gameObject);
         }
     }
 
-    private void FlashColor()
+    // 受伤闪烁
+    protected void FlashColor()
     {
         spriteRenderer.color = Color.red;
-        Invoke("ResetColor", flashTime);
+        Invoke("ResetColor", damageFlashTime);
     }
 
-    private void ResetColor()
+    // 重置颜色
+    protected void ResetColor()
     {
         spriteRenderer.color = originColor;
     }
